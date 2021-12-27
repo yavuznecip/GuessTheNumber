c5293ac0cb43de09d28c059ddf921a6c5784ec36
Necip Yavuz <1032nyavuz@gmail.com>
2021-12-27 13:42:48 +0300

    Proje dosyası ekle.

---
+++ GuessTheNumber/GuessTheNumber.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int secretNum, guess, i;
+	i = 1;
+	secretNum = rand() % 10;
+
+	cout << "Guess the secret number(0-9): " << endl;
+	cin >> guess;
+	if (guess == secretNum)
+		cout << "Conguragulations u found it at the first try!!!" << endl;
+	else {
+		do {
+			cout << "please try again: " << endl;
+			cin >> guess;
+			i++;
+
+		} while (guess != secretNum & i < 3);
+
+	    if (guess == secretNum)
+			cout << "Conguragulations u found it at the " << i << "th try! " << endl;
+		else
+			cout << "You are out of changes :(" << endl;
+	}
+}
